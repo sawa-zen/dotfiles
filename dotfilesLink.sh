ca225cd614e83dbd63650aa55bedaad89a6b3e46
sawa-zen <sawadasuiren@gmail.com>
2015-03-01 23:42:01 +0900

    dotfileを色々まとめた

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-ln -sf ~/dotfiles/.bash_profile ~/.bash_profile
+ln -sf ~/dotfiles/.zprofile ~/.zprofile
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/colors ~/.vim
