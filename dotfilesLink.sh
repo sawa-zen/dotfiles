dcfd457eeb491bb906c215d9ae4f271067805c08
sawa-zen <sawadasuiren@gmail.com>
2015-05-17 19:20:54 +0900

    eslintの追加

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -3,7 +3,7 @@ ln -sf ~/dotfiles/.zprofile ~/.zprofile
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/colors ~/.vim
-ln -sf ~/dotfiles/.jshintrc ~/.jshintrc
+ln -sf ~/dotfiles/.eslintrc ~/.eslintrc
 ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
 ln -sf ~/dotfiles/.gitignore ~/.gitignore
 ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
