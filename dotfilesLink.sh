36407ed1a575bd6cc6f933e563d63ac7679c6f19
sawa-zen <sawadasuiren@gmail.com>
2017-10-28 20:35:18 +0900

    Fix

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -7,3 +7,5 @@ ln -sf ~/dotfiles/.eslintrc ~/.eslintrc
 ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
 ln -sf ~/dotfiles/.gitignore ~/.gitignore
 ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
+ln -sf ~/dotfiles/git_diff_wrapper /usr/local/bin/git_diff_wrapper
+chmod a+x /usr/local/bin/git_diff_wrapper
