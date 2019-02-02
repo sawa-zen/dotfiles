72b4895bca4123f61ac56f6cb0e69ae6775b52de
SAWADA Takayoshi <sawadasuiren@gmail.com>
2019-02-02 22:05:57 +0900

    Remove eslintrc

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -3,7 +3,6 @@ ln -sf ~/dotfiles/.zprofile ~/.zprofile
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/colors ~/.vim/colors
-ln -sf ~/dotfiles/.eslintrc ~/.eslintrc
 ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
 ln -sf ~/dotfiles/.gitignore ~/.gitignore
 ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
