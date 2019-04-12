cb0cd2002dda8e5c1e82f82adc3d47a95c2eee45
sawa-zen <sawadasuiren@gmail.com>
2019-04-12 23:57:43 +0900

    fix

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -1,11 +1,7 @@
 #!/bin/sh
 ln -sf ~/dotfiles/.zprofile ~/.zprofile
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
-ln -sf ~/dotfiles/.vimrc ~/.vimrc
-ln -sf ~/dotfiles/colors ~/.vim/colors
 ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
 ln -sf ~/dotfiles/.gitignore ~/.gitignore
 ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
-ln -sf ~/dotfiles/git_diff_wrapper /usr/local/bin/git_diff_wrapper
 ln -sf ~/dotfiles/nvim ~/.config/nvim
-chmod a+x /usr/local/bin/git_diff_wrapper
