78727ce89ed06516d0593cd0aad04469e69e8585
SAWADA Takayoshi <sawadasuiren@gmail.com>
2019-04-13 01:07:48 +0900

    fix

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-ln -sf ~/dotfiles/.zprofile ~/.zprofile
-ln -sf ~/dotfiles/.zshrc ~/.zshrc
-ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
+ln -sf ~/dotfiles/zsh/.zprofile ~/.zprofile
+ln -sf ~/dotfiles/zsh/.zshrc ~/.zshrc
+ln -sf ~/dotfiles/tmux/.tmux.conf ~/.tmux.conf
 ln -sf ~/dotfiles/git/.gitignore_global ~/.gitignore_global
 ln -sf ~/dotfiles/git/.gitconfig ~/.gitconfig
 ln -sf ~/dotfiles/nvim ~/.config/nvim
