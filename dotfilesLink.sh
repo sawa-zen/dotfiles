4b663d9ea3805f550a5628c8e18f607b9a640c14
sawa-zen <sawadasuiren@gmail.com>
2020-09-21 18:45:34 +0900

    fix

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
-ln -sf ~/dotfiles/zsh/.zprofile ~/.zprofile
-ln -sf ~/dotfiles/zsh/.zshrc ~/.zshrc
+#ln -sf ~/dotfiles/zsh/.zprofile ~/.zprofile
+#ln -sf ~/dotfiles/zsh/.zshrc ~/.zshrc
 ln -sf ~/dotfiles/tmux/.tmux.conf ~/.tmux.conf
-ln -sf ~/dotfiles/git/.gitignore_global ~/.gitignore_global
-ln -sf ~/dotfiles/git/.gitconfig ~/.gitconfig
-ln -nsf ~/dotfiles/nvim ~/.config/nvim
+#ln -sf ~/dotfiles/git/.gitignore_global ~/.gitignore_global
+#ln -sf ~/dotfiles/git/.gitconfig ~/.gitconfig
