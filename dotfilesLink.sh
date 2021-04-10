0523aabdf29b1d63b3fbf8c69e277b58137c94c3
sawa-zen <sawadasuiren@gmail.com>
2021-04-10 17:11:48 +0900

    fix

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
+
+CURRENT=$(cd $(dirname $0);pwd)
+
 #ln -sf ~/dotfiles/zsh/.zprofile ~/.zprofile
 #ln -sf ~/dotfiles/zsh/.zshrc ~/.zshrc
-ln -sf ~/dotfiles/tmux/.tmux.conf ~/.tmux.conf
+ln -sf $CURRENT/tmux/.tmux.conf ~/.tmux.conf
 #ln -sf ~/dotfiles/git/.gitignore_global ~/.gitignore_global
 #ln -sf ~/dotfiles/git/.gitconfig ~/.gitconfig
