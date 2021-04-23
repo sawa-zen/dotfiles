3b182fbae8981a298114987263ed7d025ba2205e
sawa-zen <sawadasuiren@gmail.com>
2021-04-23 16:37:29 +0900

    fix

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -3,4 +3,5 @@
 CURRENT=$(cd $(dirname $0);pwd)
 
 ln -sf $CURRENT/tmux/.tmux.conf ~/.tmux.conf
-ln -s $CURRENT/vscode/settings.json ~/Library/Application\ Support/Code/User/settings.json
+ln -sf $CURRENT/vscode/settings.json ~/Library/Application\ Support/Code/User/settings.json
+ln -sf $CURRENT/git/.gitconfig ~/.gitconfig
