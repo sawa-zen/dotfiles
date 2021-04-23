ebf398cafdff61329408d7e7d55a2c65a741d258
sawa-zen <sawadasuiren@gmail.com>
2021-04-23 16:16:40 +0900

    fix

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -2,8 +2,5 @@
 
 CURRENT=$(cd $(dirname $0);pwd)
 
-#ln -sf ~/dotfiles/zsh/.zprofile ~/.zprofile
-#ln -sf ~/dotfiles/zsh/.zshrc ~/.zshrc
 ln -sf $CURRENT/tmux/.tmux.conf ~/.tmux.conf
-#ln -sf ~/dotfiles/git/.gitignore_global ~/.gitignore_global
-#ln -sf ~/dotfiles/git/.gitconfig ~/.gitconfig
+ln -s $CURRENT/vscode/settings.json ~/Library/Application\ Support/Code/User/settings.json
